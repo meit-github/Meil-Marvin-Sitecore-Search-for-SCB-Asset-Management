e7e5b603c1a90077f486087b5faeb049fb5d463f
Marvin Glenn Lacuna <marvsrox@gmail.com>
2024-03-17 00:31:22 +0800

    updated the static files

--- src/components/Icons/Logo.jsx
+++ src/components/Icons/Logo.jsx
@@ -1,6 +1,8 @@
 const Logo = () => {
   return (
-  <img src="https://www.scbam.com/medias/upload/home/scbam-logo-en.png" width="100%" alt="SCB Logo" />
+  // <img src="https://www.scbam.com/medias/upload/home/scbam-logo-en.png" width="100%" alt="SCB Logo" />
+  <img src="https://www.moe.gov.sg/-/media/moe/assets/moe-logo.svg" width="100%" alt="MOE Logo" />
+
   );
 };
 
--- src/pages/Home/styled.js
+++ src/pages/Home/styled.js
@@ -19,7 +19,7 @@ export const HeroRow = styled.div`
 export const HeroWrapper = styled.div`
   max-width: 1920px;
   margin: auto;
-  background-image: url(https://www.imd.org/wp-content/uploads/2023/07/20230228-CustomPrograms-SCB-TopBanner-2880x1120-AJ-1920x747.jpg);
+  background-image: url(https://static1.straitstimes.com.sg/s3fs-public/styles/large30x20/public/articles/2023/10/26/IMGL0903.JPG?h=11f638b8);
   min-height: 324px;
   height: 324px;
   background-position: center center;
