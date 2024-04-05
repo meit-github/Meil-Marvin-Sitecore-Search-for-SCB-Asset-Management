01dbd64164d62d2ef14c489b61f75729d8335965
meit-github <115689108+meit-github@users.noreply.github.com>
2024-04-05 15:17:46 +0800

    resize logo 10percent

--- src/components/Icons/Logo.jsx
+++ src/components/Icons/Logo.jsx
@@ -1,7 +1,7 @@
 const Logo = () => {
   return (
   // <img src="https://www.scbam.com/medias/upload/home/scbam-logo-en.png" width="100%" alt="SCB Logo" />
-  <img src="https://www.sentosa.gov.sg/images/who-we-are/our-brands/sentosa_logonew.png" width="20%" alt="Sentosa Logo" />
+  <img src="https://www.sentosa.gov.sg/images/who-we-are/our-brands/sentosa_logonew.png" width="10%" alt="Sentosa Logo" />
 
   );
 };
